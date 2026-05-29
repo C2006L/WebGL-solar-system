29325a9c0cc113d75e84112a7d47d948cfe079fe
Chris Lee <15853831797@163.com>
2026-05-30 02:28:56 +0800

    quality: shadow map max quality 8192

--- js/lighting.js
+++ js/lighting.js
@@ -21,8 +21,8 @@ export function createLighting() {
   const sunLight = new THREE.PointLight("#fffef5", 280, 0, 1);
   sunLight.position.set(0, 0, 0);
   sunLight.castShadow = true;
-  sunLight.shadow.mapSize.width = 2048;
-  sunLight.shadow.mapSize.height = 2048;
+  sunLight.shadow.mapSize.width = 8192;
+  sunLight.shadow.mapSize.height = 8192;
   sunLight.shadow.camera.near = 0.15;
   sunLight.shadow.camera.far = 120;
   sunLight.shadow.bias = -0.0003;
